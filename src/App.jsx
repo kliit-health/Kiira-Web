18592f8ec145988117b3803991c2d42e770de469
Kasope Abolade <aboladekasope@gmail.com>
2023-05-10 11:03:45 +0100

    REPO SETUP: Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="font-bold text-4xl text-center text-blue-800">
+        Welcome to Kiira Health Management Portal
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import macrosPlugin from "vite-plugin-babel-macros";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  esbuild: {
+    jsxFactory: "jsx",
+    jsxInject: `
+      import { jsx } from '@emotion/react';
+    `,
+  },
+  plugins: [macrosPlugin(), react()],
+  server: {
+    port: 3000,
+  },
+});
