976b9d85cae5c22af4e16af0e7b16e1b66d5cf62
Kasope Abolade <aboladekasope@gmail.com>
2023-05-19 10:00:35 +0100

    BUG: vite build config update

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import macrosPlugin from 'vite-plugin-babel-macros';
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    jsxFactory: 'jsx',
-    jsxInject: `
-    import { jsx } from '@emotion/react';
-  `
-  },
   plugins: [macrosPlugin(), react(), svgr()],
   server: {
     port: 3000
