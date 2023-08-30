10d627aeb8d30a046091fdd3a287dd7ff8446a9c
Kasope Abolade <aboladekasope@gmail.com>
2023-08-30 14:47:39 +0100

    WIP: sandbox credentials

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,8 +3,8 @@ export const genderOptions = [
   { label: 'Female', value: 'Female' }
 ];
 
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
-export const APP_URL = import.meta.env.VITE_APP_URL;
-export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+export const BASE_URL = import.meta.env.VITE_BASE_URL_SANDBOX
+export const APP_URL = import.meta.env.VITE_APP_URL_SANDBOX;
+export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID_SANDBOX;
 export const MIXED_PANEL_TOKEN = import.meta.env.VITE_MIXED_PANEL_TOKEN;
-export const STRIPE_PK = import.meta.env.VITE_STRIPE_PK;
+export const STRIPE_PK = import.meta.env.VITE_STRIPE_PK_SANDBOX;
